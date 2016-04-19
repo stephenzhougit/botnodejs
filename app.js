180548c6de2e3fb2bf345f4fe923f92b5109b023
stephenzhougit <stephenzhou@yahoo.com>
2016-04-18 18:13:47 -0700

    Initial commit.

---
+++ app.js
@@ -0,0 +1,57 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Create bot and add dialogs
+var bot = new builder.BotConnectorBot({ appId: 'YourAppId', appSecret: 'YourAppSecret' });
+// bot.add('/', function (session) {
+//     session.send('Hello World');
+// });
+
+
+// bot.add('/', function (session) {
+//     if (!session.userData.name) {
+//         session.beginDialog('/profile');
+//     } else {
+//         session.send('Hello %s!', session.userData.name);
+//     }
+// });
+// bot.add('/profile', [
+//     function (session) {
+//         builder.Prompts.text(session, 'Hi! What is your name?');
+//     },
+//     function (session, results) {
+//         session.userData.name = results.response;
+//         session.endDialog();
+//     }
+// ]);
+
+bot.add('/', new builder.CommandDialog()
+    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
+    .matches('^quit', builder.DialogAction.endDialog())
+    .onDefault(function (session) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            session.send('Hello %s!', session.userData.name);
+        }
+    }));
+bot.add('/profile',  [
+    function (session) {
+        if (session.userData.name) {
+            builder.Prompts.text(session, 'What would you like to change it to?');
+        } else {
+            builder.Prompts.text(session, 'Hi! What is your name?');
+        }
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
